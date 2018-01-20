bec0e694c4fdd8851ce2ce732f3bc79a32c17ade
钟鑫 <490336534@qq.com>
2018-01-20 08:27:54 +0800

    Update note.sql
    开启日志

--- note.sql
+++ note.sql
@@ -1,2 +1,9 @@
 SELECT FOUND_ROWS();
 SELECT COUNT(1) FROM TmpVideo;
+
+#开启数据库日志
+set GLOBAL general_log = 1;
+#数据库日志保存到数据库里，默认表在mysql.general_log
+set GLOBAL log_output = 'TABLE';
+#查登陆失败日志的语句
+SELECT * FROM mysql.general_log where command_type='Connect'
